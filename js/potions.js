164cf457ff0d51331bc2166f7464efdebbe4d58f
Zientek Patrik <PZientek@cpas.cz>
2019-09-04 19:07:07 +0200

    Added option to roll 0 or multiple side effects

--- js/potions.js
+++ js/potions.js
@@ -37,7 +37,7 @@ $(document).ready(function (){
 
     function GeneratePotion () {
         const quantity = $("#potion_quantity").val();
-        const sideffects = $("#potion_sideeffects").val();
+        let sideeffects = $("#potion_sideeffects").val();
         const spoiledChance = $("#potion_spoiledchance").val();
         let spoiled = 0;
         let mainBody = $("#potions_results");
@@ -53,23 +53,28 @@ $(document).ready(function (){
             } else {
                 spoiledStart = spoiledEnd = "";
             }
-            element = "<h4>Potion " + (i+1) + ":</h4><p>" + 
-                        "<b>Type:</b> " + potion_type[Math.floor(Math.random() * potion_type.length)] + "<br>" +
-                        "<b>Main effect:</b> " + potion_mainEffect[Math.floor(Math.random() * potion_mainEffect.length)] + "<br>" +
-                        "<b>Strength:</b> " + potion_strength[Math.floor(Math.random() * potion_strength.length)] + "<br>" +
-                        "<b>Side effects:</b> " + potion_sideEffect[Math.floor(Math.random() * potion_sideEffect.length)] + "<br>" +
-                        "<b>Container:</b> " + potion_container[Math.floor(Math.random() * potion_container.length)] + "<br>" +
-                        "<b>Appearance:</b> " + potion_appearance1[Math.floor(Math.random() * potion_appearance1.length)] + " with " + potion_appearance2[Math.floor(Math.random() * potion_appearance2.length)].toLowerCase() + "<br>" +
-                        "<b>Texture:</b> " + potion_texture[Math.floor(Math.random() * potion_texture.length)] + "<br>" +
-                        "<b>Taste:</b> " + potion_taste[Math.floor(Math.random() * potion_taste.length)] + "<br>" +
-                        "<b>Smell:</b> " + potion_taste[Math.floor(Math.random() * potion_taste.length)] + "<br>" +
-                        "<b>Label:</b> " + potion_label[Math.floor(Math.random() * potion_label.length)] + "</p>";
+            element = "<h4>Potion " + (i+1) + ":</h4><p>";
+            let type = "<b>Type:</b> " + potion_type[Math.floor(Math.random() * potion_type.length)] + "<br>";
+            let mainEffect = "<b>Main effect:</b> " + potion_mainEffect[Math.floor(Math.random() * potion_mainEffect.length)] + "<br>";
+            let strength = "<b>Strength:</b> " + potion_strength[Math.floor(Math.random() * potion_strength.length)] + "<br>";
+            let sideEffect = "<b>Side effects:</b> ";
+            if (sideeffects > potion_sideEffect.length) sideeffects = potion_sideEffect.length;
 
-            // if (sideffects !== "0") {
-            //     element += ", bad stuff";
-            // }
-            // element += ", description</p>";
-            resultElement = spoiledStart + element + spoiledEnd;
+            let tmpArray = potion_sideEffect;
+            for (let j = 0; j < sideeffects; j++) {
+                let tmp = tmpArray.splice(Math.floor(Math.random() * tmpArray.length), 1);
+                sideEffect += tmp[0].substring(0, tmp[0].length - 1) + ", ";
+            }
+            if (sideEffect !== "<b>Side effects:</b> ") sideEffect = sideEffect.substring(0, sideEffect.length - 2);
+            sideEffect += "<br>";
+            let container = "<b>Container:</b> " + potion_container[Math.floor(Math.random() * potion_container.length)] + "<br>";
+            let appearance = "<b>Appearance:</b> " + potion_appearance1[Math.floor(Math.random() * potion_appearance1.length)] + " with " + potion_appearance2[Math.floor(Math.random() * potion_appearance2.length)].toLowerCase() + "<br>";
+            let texture = "<b>Texture:</b> " + potion_texture[Math.floor(Math.random() * potion_texture.length)] + "<br>";
+            let taste = "<b>Taste:</b> " + potion_taste[Math.floor(Math.random() * potion_taste.length)] + "<br>";
+            let smell = "<b>Smell:</b> " + potion_taste[Math.floor(Math.random() * potion_taste.length)] + "<br>";
+            let label = "<b>Label:</b> " + potion_label[Math.floor(Math.random() * potion_label.length)] + "</p>";
+            
+            resultElement = spoiledStart + element + type + mainEffect + strength + sideEffect + container + appearance + texture + taste + smell + label + spoiledEnd;
             mainBody.append(resultElement);
             history.append(resultElement);    
         } 
